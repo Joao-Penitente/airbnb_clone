49a96ff59f92e5d9d288cd48c0be086eb3ef33a3
Joao Penitente <joaov.penitente@gmail.com>
2023-05-26 12:22:13 -0300

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,55 @@
-export default function Home() {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import getCurrentUser from '@/actions/getCurrentUser'
+import getListings, { IListingsParams } from '@/actions/getListings'
+import ClientOnly from '@/components/ClientOnly'
+import Container from '@/components/Container'
+import EmptyState from '@/components/EmptyState'
+import ListingCard from '@/components/listings/ListingCard'
+
+export const dynamic = 'force-static'
+interface HomeProps {
+	searchParams: IListingsParams
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+	const listings = await getListings(searchParams)
+	const currentUser = await getCurrentUser()
+
+	if (listings.length === 0) {
+		return (
+			<ClientOnly>
+				<EmptyState showReset />
+			</ClientOnly>
+		)
+	}
+
 	return (
-		<>
-			<h2>zerado</h2>
-		</>
+		<ClientOnly>
+			<Container>
+				<div
+					className="
+              pt-24
+              grid 
+              grid-cols-1 
+              sm:grid-cols-2 
+              md:grid-cols-3 
+              lg:grid-cols-4
+              xl:grid-cols-5
+              2xl:grid-cols-6
+              gap-8
+            "
+				>
+					{listings.map((listing: any) => (
+						<ListingCard
+							currentUser={currentUser}
+							key={listing.id}
+							data={listing}
+						/>
+					))}
+				</div>
+			</Container>
+		</ClientOnly>
 	)
 }
+
+export default Home
