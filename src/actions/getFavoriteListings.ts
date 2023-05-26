417cb4e6e93c155c259d4ddb9280e25f32ed6fe4
Joao Penitente <joaov.penitente@gmail.com>
2023-05-26 12:20:55 -0300

    feat: add action getFavoriteListings

---
+++ src/actions/getFavoriteListings.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import prisma from '@/libs/prismadb'
+
+import getCurrentUser from './getCurrentUser'
+
+export default async function getFavoriteListings() {
+	try {
+		const currentUser = await getCurrentUser()
+		if (!currentUser) return []
+
+		const favorites = await prisma.listing.findMany({
+			where: {
+				id: {
+					in: [...(currentUser.favoriteIds || [])]
+				}
+			}
+		})
+
+		const safeFavorites = favorites.map((favorite) => {
+			return {
+				...favorite,
+				createdAt: favorite.createdAt.toISOString()
+			}
+		})
+
+		return safeFavorites
+	} catch (error: any) {
+		throw new Error(error)
+	}
+}
