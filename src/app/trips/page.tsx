03679c51ac09b0869f684b792a9a19e1e429d0ae
Joao Penitente <joaov.penitente@gmail.com>
2023-05-26 12:25:43 -0300

    feat: add trips page

---
+++ src/app/trips/page.tsx
@@ -0,0 +1,41 @@
+import getCurrentUser from '@/actions/getCurrentUser'
+import getReservations from '@/actions/getReservations'
+import ClientOnly from '@/components/ClientOnly'
+import EmptyState from '@/components/EmptyState'
+
+import TripsClient from './TripsClient'
+
+const TripsPage = async () => {
+	const currentUser = await getCurrentUser()
+	if (!currentUser) {
+		return (
+			<ClientOnly>
+				<EmptyState title="Unauthorized" subtitle="Please Login" />
+			</ClientOnly>
+		)
+	}
+
+	const reservations = await getReservations({ userId: currentUser.id })
+
+	if (reservations.length == 0) {
+		return (
+			<ClientOnly>
+				<EmptyState
+					title="No trips found"
+					subtitle="Looks like you haven't reserved any trips."
+				/>
+			</ClientOnly>
+		)
+	}
+
+	return (
+		<ClientOnly>
+			<TripsClient
+				reservations={reservations}
+				currentUser={currentUser}
+			/>
+		</ClientOnly>
+	)
+}
+
+export default TripsPage
