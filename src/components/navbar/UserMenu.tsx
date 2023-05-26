d9887c45e2100e118a94b7c68e0ce495a8884a88
Joao Penitente <joaov.penitente@gmail.com>
2023-05-26 12:30:14 -0300

    feat: add UserMenu Component

---
+++ src/components/navbar/UserMenu.tsx
@@ -0,0 +1,112 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+'use client'
+
+import { signOut } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import { useCallback, useState } from 'react'
+import { AiOutlineMenu } from 'react-icons/ai'
+
+import useLoginModal from '@/hooks/useLoginModal'
+import useRegisterModal from '@/hooks/useRegisterModal'
+import useRentModal from '@/hooks/useRentModal'
+import { SafeUser } from '@/types'
+
+import Avatar from '../Avatar'
+import MenuItem from './MenuItem'
+
+interface UserMenuProps {
+	currentUser?: SafeUser | null
+}
+
+const UserMenu = ({ currentUser }: UserMenuProps) => {
+	const registerModal = useRegisterModal()
+	const loginModal = useLoginModal()
+	const rentModal = useRentModal()
+	const router = useRouter()
+
+	const [isOpen, setIsOpen] = useState(false)
+
+	const toggleOpen = useCallback(() => {
+		setIsOpen((value) => !value)
+	}, [])
+
+	const onRent = useCallback(() => {
+		if (!currentUser) {
+			return loginModal.onOpen()
+		}
+
+		rentModal.onOpen()
+	}, [currentUser, loginModal, rentModal])
+
+	return (
+		<div className="relative">
+			<div className="flex flex-row items-center gap-3">
+				<button
+					onClick={onRent}
+					className="hidden md:block text-sm font-semibold py-4 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer "
+				>
+					Airbnb your home
+				</button>
+				<div
+					onClick={toggleOpen}
+					className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition "
+				>
+					<AiOutlineMenu />
+					<div className="hidden md:block">
+						<Avatar src={currentUser?.image} />
+					</div>
+				</div>
+			</div>
+			{isOpen && (
+				<div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm ">
+					<div className="flex flex-col cursor-pointer">
+						{currentUser ? (
+							<>
+								<MenuItem
+									onClick={() => router.push('/trips')}
+									label="My trips"
+								/>
+								<MenuItem
+									onClick={() => router.push('/favorites')}
+									label="My favorites"
+								/>
+								<MenuItem
+									onClick={() => router.push('/reservations')}
+									label="My reservations"
+								/>
+								<MenuItem
+									onClick={() => router.push('/properties')}
+									label="My properties"
+								/>
+								<MenuItem
+									onClick={rentModal.onOpen}
+									label="Airbnb my home"
+								/>
+								<hr />
+								<MenuItem
+									onClick={() => {
+										signOut()
+									}}
+									label="Logout"
+								/>
+							</>
+						) : (
+							<>
+								<MenuItem
+									onClick={loginModal.onOpen}
+									label="Login"
+								/>
+								<MenuItem
+									label="Sign up"
+									onClick={registerModal.onOpen}
+								/>
+							</>
+						)}
+					</div>
+				</div>
+			)}
+		</div>
+	)
+}
+
+export default UserMenu
