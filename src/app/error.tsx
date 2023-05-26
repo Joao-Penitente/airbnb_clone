b344d9ac34a0bb1579539972354e3624652fc2ac
Joao Penitente <joaov.penitente@gmail.com>
2023-05-26 12:22:45 -0300

    feat: add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import EmptyState from '@/components/EmptyState'
+
+interface ErrorStateProps {
+	error: Error
+}
+
+const ErrorState = ({ error }: ErrorStateProps) => {
+	useEffect(() => {
+		console.error(error)
+	}, [error])
+	return <EmptyState title="Uh Oh" subtitle="Something went wrong!" />
+}
+
+export default ErrorState
