130c31398bcc96ef982c20f47312105967e21c4a
João vitor dos Santos Penitente <joaov.penitente@gmail.com>
2023-05-26 16:52:43 -0300

    fix

--- src/components/listings/ListingInfo.tsx
+++ src/components/listings/ListingInfo.tsx
@@ -1,39 +1,42 @@
 'use client'
+
 import dynamic from 'next/dynamic'
 import { IconType } from 'react-icons'
 
-import useCountries from '@/hooks/useCountries'
-import { SafeUser } from '@/types'
-
 import Avatar from '../Avatar'
 import ListingCategory from './ListingCategory'
+import { SafeUser } from '@/types'
+import useCountries from '@/hooks/useCountries'
 
 const Map = dynamic(() => import('../Map'), {
 	ssr: false
 })
+
 interface ListingInfoProps {
 	user: SafeUser
 	description: string
-	roomCount: number
 	guestCount: number
+	roomCount: number
 	bathroomCount: number
+	category:
+		| {
+				icon: IconType
+				label: string
+				description: string
+		  }
+		| undefined
 	locationValue: string
-	category: {
-        icon: IconType,
-        label: string;
-        description: string;
-      } | undefined
 }
 
 const ListingInfo = ({
 	user,
-	category,
 	description,
-	roomCount,
 	guestCount,
+	roomCount,
 	bathroomCount,
+	category,
 	locationValue
-}: ListingInfoProps) => {
+}:ListingInfoProps) => {
 	const { getByValue } = useCountries()
 
 	const coordinates = getByValue(locationValue)?.latlng
@@ -41,26 +44,47 @@ const ListingInfo = ({
 	return (
 		<div className="col-span-4 flex flex-col gap-8">
 			<div className="flex flex-col gap-2">
-				<div className="text-xl font-semibold flex flex-row items-center gap-2">
+				<div
+					className="
+                    text-xl 
+                    font-semibold 
+                    flex 
+                    flex-row 
+                    items-center
+                    gap-2
+                "
+				>
 					<div>Hosted by {user?.name}</div>
 					<Avatar src={user?.image} />
 				</div>
-				<div className="flex flex-row items-center gap-4 font-light text-neutral-500 ">
+				<div
+					className="
+                    flex 
+                    flex-row 
+                    items-center 
+                    gap-4 
+                    font-light
+                    text-neutral-500
+                 "
+				>
 					<div>{guestCount} guests</div>
 					<div>{roomCount} rooms</div>
-					<div>{bathroomCount} bathroom</div>
+					<div>{bathroomCount} bathrooms</div>
 				</div>
 			</div>
 			<hr />
 			{category && (
 				<ListingCategory
 					icon={category.icon}
-					label={category.label}
-					description={category.description}
+					label={category?.label}
+					description={category?.description}
 				/>
 			)}
 			<hr />
-			<div className="text-lg font-light text-neutral-500">
+			<div
+				className="
+                text-lg font-light text-neutral-500"
+			>
 				{description}
 			</div>
 			<hr />
